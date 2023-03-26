d85777e5672a9dd89b89edc06d1a471911d446e8
Jamie Olsen <jamieolsen12@gmail.com>
2023-03-26 15:52:20 +0700

    project starting files

---
+++ app.js
@@ -0,0 +1,29 @@
+// Plan // 
+
+//1. Create functions for basic operators 
+    // add
+    // subtract
+    // multiply
+    // divide
+//1.1 test in browser console
+
+//2. Create variables for 3 sections of an operation
+//      variable a - first number
+//      variable b - operator
+//      variable c - second number
+
+// 3. Create a new function operate that takes an operator and 2 numbers 
+//      and then calls one of the above functions on the numbers.
+// 
+// 4. Create calculator GUI in HTML
+//      4.1 Don't worry about it looking good
+//      4.2 Just make sure it works and can be customized later
+//      4.3 Make sure current operation is displayed correctly
+//      4.4 Add clear button
+// 5. Make the calculator work! 
+//      5.1 You’ll need to store the first number that is input into the calculator when a user 
+//          presses an operator, and also save which operation has been chosen and then operate() 
+//          on them when the user presses the “=” key.
+//      5.2 You should already have the code that can populate the display, so once operate() has been called, 
+//          update the display with the ‘solution’ to the operation.
+//      5.3 
