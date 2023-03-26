f13a734805b1af44b4e3ef05faebd34c01e4f59e
Jamie Olsen <jamieolsen12@gmail.com>
2023-03-26 16:07:42 +0700

    Create basic operator functions add, subtract, multiply, divide. Create operator function which takes 3 inputs of operator, num1, num2 and calls relevant functions to solve operation

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // Plan // 
 
-//1. Create functions for basic operators 
+// 1. Create functions for basic operators 
     // add
     // subtract
     // multiply
     // divide
-//1.1 test in browser console
-
-//2. Create variables for 3 sections of an operation
+// 1.1 test in browser console
+//
+// 2. Create variables for 3 sections of an operation
 //      variable a - first number
 //      variable b - operator
 //      variable c - second number
-
+//
 // 3. Create a new function operate that takes an operator and 2 numbers 
 //      and then calls one of the above functions on the numbers.
 // 
@@ -26,4 +26,50 @@
 //          on them when the user presses the “=” key.
 //      5.2 You should already have the code that can populate the display, so once operate() has been called, 
 //          update the display with the ‘solution’ to the operation.
-//      5.3 
+//      5.3 This part is hard
+// 6. Your calculator should not evaluate more than a single pair of numbers at a time.
+
+
+
+// Operator Functions
+
+function add(a, b) {
+    return (a + b)
+}
+
+function subtract(a, b) {
+    return (a - b)
+}
+
+function multiply(a, b) {
+    return (a * b)
+}
+
+function divide(a, b) {
+    return (a / b)
+}
+
+function operate(operator, num1, num2) {
+    let result;
+
+    switch(operator){
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+        default:
+            console.log("Invalid Operator.");
+    }
+    return result;
+}
+
+
+
